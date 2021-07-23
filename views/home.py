2506d759e0a9e1118d3601cf3f29c50b955476cd
Ruslan Ishtuganov <7631314+ruslaniv@users.noreply.github.com>
2021-07-23 19:09:33 +0700

    Add env file

--- views/home.py
+++ views/home.py
@@ -1,8 +1,13 @@
+import os
 import aiohttp_jinja2
 from aiohttp import web
+from dotenv import load_dotenv
+
+load_dotenv()
+user = os.environ.get("POSTGRES_USER")
 
 
 class HomePageView(web.View):
     @aiohttp_jinja2.template('home.jinja2')
     async def get(self) -> dict:
-        return {"user": "Ruslan"}
+        return {"user": user}
